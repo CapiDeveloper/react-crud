825affa3dfa1dc27c7d83c991ef05be8cd1e7d02
Bryan Chacha <morocho.12chachas@gmail.com>
2023-12-15 09:50:54 -0500

    Primer commit

---
+++ src/data/tours.js
@@ -0,0 +1,20 @@
+const data = [
+    {
+        imagen:'https://naturegalapagos.com/wp-content/uploads/2016/06/Daphne-Galapagos-Cruise-Trip.jpg',
+        crucero:'Daphne Galapagos Cruise',
+        salida:'23 de Enero del 2024',
+        llegada:'28 de Enero del 2024',
+        itinerario:'5D / 4N Itinerary A5',
+        precio: 2.258,
+        formulario:'https://landing.mailerlite.com/webforms/landing/q4a5l9',
+    },
+    {
+        imagen:'https://naturegalapagos.com/wp-content/uploads/2016/06/Daphne-Galapagos-Cruise-Trip.jpg',
+        crucero:'Daphne Galapagos Cruise',
+        salida:'20 de Enero del 2024',
+        llegada:'24 de Enero del 2024',
+        itinerario:'4D / 3N Itinerary A4',
+        precio: 2.158,
+        formulario:'https://landing.mailerlite.com/webforms/landing/q4a5l9',
+    }
+]
